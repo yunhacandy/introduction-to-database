23a06c7761c3d7e43dc6284cd02eab9e9cf27868
yunhacandy <yunhacandy@gmail.com>
2024-11-20 11:17:16 +0900

    feat: 실습10 기본 세팅

---
+++ loginProject/src/test/java/hongik/loginProject/LoginProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.loginProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
